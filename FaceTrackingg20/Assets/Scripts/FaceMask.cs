840426139b1f5473afb0937c89a363e98530c624
Adhiraj <adhirajsingh0105@gmail.com>
2021-01-22 12:42:12 +0530

    Adding Files
    Occlusion is not done

---
+++ FaceTrackingg20/Assets/Scripts/FaceMask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class FaceMask : MonoBehaviour
+{
+    public GameObject faceMask;
+    private List<AugmentedFace> _tempFaceMask = new List<AugmentedFace>();
+
+    
+    void Update()
+    {
+        Session.GetTrackables(_tempFaceMask, TrackableQueryFilter.All);
+
+        if (_tempFaceMask.Count == 0)
+        {
+            faceMask.SetActive(false);
+        }
+        else
+        {
+            faceMask.SetActive(true);
+        }
+    }
+}
